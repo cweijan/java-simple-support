b5a5d3d9a68b515eeaad7cae0fc7d577a98d7e6f
cweijan <weijancc@gmail.com>
2025-04-22 15:48:24 +0800

    Support parse for loop variable

--- src/parser/visitors/members/methodVisitor.ts
+++ src/parser/visitors/members/methodVisitor.ts
@@ -18,7 +18,8 @@ export class MethodVisitor extends MemberVisitor {
                 const methodSymbol = this.createSymbolWithType('method', ctx, typeName);
                 methodSymbol.children = [
                     ...this.parseMethodParameters(ctx),
-                    ...this.parseLocalVariables(ctx)
+                    ...this.parseLocalVariables(ctx),
+                    ...this.parseLoopVariables(ctx)
                 ];
                 this.symbols.push(methodSymbol);
                 return 1;
@@ -82,4 +83,26 @@ export class MethodVisitor extends MemberVisitor {
         }
         return localVariables;
     }
+
+    private parseLoopVariables(ctx: MethodDeclarationContext): JavaSymbol[] {
+        const loopVariables: JavaSymbol[] = [];
+        const methodBody = ctx.methodBody();
+        if (methodBody) {
+            const loopVisitor = createVisitor({
+                visitEnhancedForControl: (ctx) => {
+                    const typeType = ctx.typeType();
+                    const typeName = typeType?.text || '';
+                    const variableDeclaratorId = ctx.variableDeclaratorId();
+                    if (variableDeclaratorId) {
+                        const localVariableSymbol = this.createSymbolWithType('localVariable', variableDeclaratorId, typeName);
+                        loopVariables.push(localVariableSymbol);
+                    }
+                    return 1;
+                }
+            });
+            loopVisitor.visit(methodBody);
+        }
+        return loopVariables;
+    }
+
 } 
